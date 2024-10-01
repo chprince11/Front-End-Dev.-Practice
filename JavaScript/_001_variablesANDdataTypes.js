e44d128c9260d9a5a3b6cf6d88684516078901e3
chprince11 <chprince@sheridancollege.ca>
2024-10-01 00:18:48 -0400

    Added varibalesJS

--- JavaScript/_001_variablesANDdataTypes.js
+++ JavaScript/_001_variablesANDdataTypes.js
@@ -42,3 +42,35 @@ const my_first_name = "Prince"; // Snake Case
 const MY_FIRST_NAME = "Prince"; // Constant Case
 const myfirstname = "Prince"; // lower Case
 const MyFirstName = "Prince"; // Pascal Case
+
+// Varibale name should be meaningful, wrong using a, ex: const a = 45; cosnt age = 30;
+
+// varibale expression
+const age = 23;
+const brotherAge = 25;
+// ; (semicolon) - It represent the end of an expression, so it is always good to use semicolon at the end of every expression
+
+// Data Types in JS
+
+// Primitive Data Types
+
+const myAge = 23; // Number
+const myName = "Prince Chauhan"; // String
+const canDriveCar = true; // Boolean
+console.log(typeof myAge); // number
+
+const age1 = 12;
+const age2 = "12";
+// Implicit casting
+console.log(age1 + age2); //1212
+// Explicit Casting
+console.log(age1 + Number(age2)); //24
+console.log(String(age1) + age2); //
+
+// String
+
+const myFullName = "Prince Chauhan";
+// const myFullName2 = 'Prince Chauhan'; 
+// This is valid
+// const myFullName3 = 'Prince Chauhan"; 
+// ERROR can't use single and double quatation in same String
