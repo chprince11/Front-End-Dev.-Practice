e75b962860e69f6fcf3c09ebf184d6441d4deeb0
chprince11 <chprince@sheridancollege.ca>
2024-10-02 00:21:23 -0400

    inital commit

--- freeCodeCamp.org/JavaScript/_001_variables_and_data_types.js
+++ freeCodeCamp.org/JavaScript/_001_variables_and_data_types.js
@@ -141,3 +141,4 @@ const myFirstFunction = function () {
 };
 
 console.log(myFirstFunction());
+
---
+++ freeCodeCamp.org/JavaScript/_002_operators.js
@@ -0,0 +1,14 @@
+// Arithmetic operators
+// + , - , * , / , % , ** , ++, --
+
+console.log(20 + 20);
+console.log(20 - 20);
+console.log(20 / 20);
+console.log(20 * 20);
+console.log(2 ** 3);
+console.log(10 % 2); // return the remainder
+let a = 5;
+console.log(a++);
+console.log(++a);
+console.log(--a);
+console.log(a--);
